4f68b68660cc6f6b9cf117f8b51ea095976f8fcf
Sebastian Johansson <sebben.97@hotmail.com>
2019-11-19 12:11:25 +0100

    Added some styling

--- public/index.js
+++ public/index.js
@@ -1,16 +1,15 @@
-let key = "f3860499b7637297d98274c59e0c47f9"
-
 function getWeather(){
     let currentCity = document.getElementById("citySearched")
     let temperature = document.getElementById("temperature")
+    let weather = document.getElementById("weather")
     let city = document.getElementById("city").value
     
     fetch(`http://localhost:3000/city/${city.length ? city : "Gothenburg"}`)
         .then(res => res.json())
         .then(data => {
-            currentCity.innerHTML = data.name
-            temperature.innerHTML = Math.round(data.main.temp)
-            console.log(data)
+            currentCity.innerHTML = "Choosen city:<br>" + data.name
+            temperature.innerHTML = "Current temperature:<br>" + Math.round(data.main.temp) + "&#8451"
+            weather.innerHTML = "Current weather:<br>" + data.weather[0].main
         }).
         catch(error => {
             console.log(error)
@@ -24,11 +23,17 @@ function getJoke(){
     fetch(`http://localhost:3000/joke`)
         .then(res => res.json())
         .then(data => {
-            question.innerHTML = data[0].setup
-            setTimeout( () => {
-                punchline.innerHTML = data[0].punchline
-            }, 2000)
-            console.log(data)
+            if(!question.innerHTML && !punchline.innerHTML){
+                question.innerHTML = data[0].setup
+                question.scrollIntoView()
+                setTimeout( () => {
+                    punchline.innerHTML = data[0].punchline
+                    punchline.scrollIntoView()
+                }, 2000)
+            }else {
+                question.innerHTML = ""
+                punchline.innerHTML = ""
+            }
         }).
         catch(error => {
             console.log(error)
