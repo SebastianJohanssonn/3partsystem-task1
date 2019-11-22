60bc1bc4d27603e8399ca3b655a1ed2b0c057a6e
Sebastian Johansson <sebben.97@hotmail.com>
2019-11-22 14:02:10 +0100

    added async to functions

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
-function getWeather(){
+async function getWeather(){
     let currentCity = document.getElementById("citySearched")
     let temperature = document.getElementById("temperature")
     let weather = document.getElementById("weather")
     let city = document.getElementById("city").value
     
-    fetch(`http://localhost:3000/city/${city.length ? city : "Gothenburg"}`)
+    await fetch(`http://localhost:3000/city/${city.length ? city : "Gothenburg"}`)
         .then(res => res.json())
         .then(data => {
             currentCity.innerHTML = "City:<br>" + data.name
@@ -17,11 +17,11 @@ function getWeather(){
         })
 }
 
-function getJoke(){
+async function getJoke(){
     let question = document.getElementById("question")
     let punchline = document.getElementById("punchline")
     
-    fetch(`http://localhost:3000/joke`)
+    await fetch(`http://localhost:3000/joke`)
         .then(res => res.json())
         .then(data => {
             if(question.innerHTML && punchline.innerHTML){
